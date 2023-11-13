15d96662bfe4db95a785819c975d215eb2866691
smoothjass <100789381+smoothjass@users.noreply.github.com>
2023-11-13 11:58:38 +0100

    - added logging

--- src/main/java/org/openapitools/services/requestservices/MessageService.java
+++ src/main/java/org/openapitools/services/requestservices/MessageService.java
@@ -2,6 +2,8 @@ package org.openapitools.services.requestservices;
 
 import org.openapitools.configuration.RabbitMQConfig;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageService {
     private final RabbitTemplate rabbit;
-
+    Logger log = LoggerFactory.getLogger(MessageService.class);
     @Autowired
     public MessageService(RabbitTemplate rabbit) {
         this.rabbit = rabbit;
@@ -18,6 +20,7 @@ public class MessageService {
 
     @RabbitListener(queues = RabbitMQConfig.ECHO_IN_QUEUE_NAME)
     public void receive(String in) {
+        log.info("MessageService received: '" + in + "'");
         System.out.println("MessageService received: '" + in + "'");
     }
 }
