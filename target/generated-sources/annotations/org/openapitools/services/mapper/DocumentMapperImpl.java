6e514e80fd49725ed5131c2ab21014b71a398a1a
smoothjass <100789381+smoothjass@users.noreply.github.com>
2024-01-09 12:12:44 +0100

    added some tests, total 10 green tests

--- target/generated-sources/annotations/org/openapitools/services/mapper/DocumentMapperImpl.java
+++ target/generated-sources/annotations/org/openapitools/services/mapper/DocumentMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-08T18:04:26+0100",
+    date = "2024-01-09T12:04:50+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21 (SAP SE)"
 )
 @Component
